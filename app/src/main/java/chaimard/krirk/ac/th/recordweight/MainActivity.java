425e80dc31ac8671cd8efbe5b148cf3ff6d5cc01
chaimard <chaimard@email.krirk.ac.th>
2018-01-30 11:58:22 +0700

    Finished 30 jan 18

--- app/src/main/java/chaimard/krirk/ac/th/recordweight/MainActivity.java
+++ app/src/main/java/chaimard/krirk/ac/th/recordweight/MainActivity.java
@@ -3,11 +3,24 @@ package chaimard.krirk.ac.th.recordweight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import chaimard.krirk.ac.th.recordweight.utility.MyManager;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MyManager myManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+
+        myManager = new MyManager(MainActivity.this);
+
+
+
+
+    }   // Main Method
+}   // Main Class
