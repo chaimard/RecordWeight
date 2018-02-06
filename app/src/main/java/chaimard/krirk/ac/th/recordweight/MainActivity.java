baac1df044d6c970eb60c639c3b3a3e2fb1da0c8
chaimard <chaimard@email.krirk.ac.th>
2018-02-06 12:06:01 +0700

    finished 6feb

--- app/src/main/java/chaimard/krirk/ac/th/recordweight/MainActivity.java
+++ app/src/main/java/chaimard/krirk/ac/th/recordweight/MainActivity.java
@@ -1,9 +1,11 @@
 package chaimard.krirk.ac.th.recordweight;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import chaimard.krirk.ac.th.recordweight.utility.MyManager;
+import chaimard.krirk.ac.th.recordweight.utility.MyOpenHelperSQLite;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,12 +21,42 @@ public class MainActivity extends AppCompatActivity {
 
         myManager = new MyManager(MainActivity.this);
 
-//        Test Add User
-        myManager.addValueToUserrTable("nick",
-                "start", "weight", "height",
-                "gender", "age");
+//        Test Add User    เดิมสร้างอันนี้ก่อน  เสร็จแล้ว ctrl+alt+M เพื่อสร้าง testAddUser ด้านล่างแล้ว remark
+//        myManager.addValueToUserrTable("nick",
+//                "start", "weight", "height",
+//                "gender", "age");
+
+//        testAddUser();
+//---------------------------------
+
+        //สร้าง delete all usertable
+
+//        Delete All userTable
+        //-------------
+//        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelperSQLite.databaseName,
+//                MODE_PRIVATE, null);     //mode private คือลยข้างในเท่านั้น
+//
+//        sqLiteDatabase.delete("userTABLE", null, null);
+
+        //----------- ทำช่วงนี้เสร็จแล้ว  36-39 แล้วให้ ctrl+alt+M เพื่อสร้าง method ภายนอก 50-55 แล้วไป remark บรรทัด 44
+
+
+        //deleteAllUserTable();
 
 
 
     }   // Main Method
+
+    private void deleteAllUserTable() {
+        SQLiteDatabase sqLiteDatabase = openOrCreateDatabase(MyOpenHelperSQLite.databaseName,
+                MODE_PRIVATE, null);     //mode private คือลยข้างในเท่านั้น
+
+        sqLiteDatabase.delete("userTABLE", null, null);
+    }
+
+    private void testAddUser() {
+        myManager.addValueToUserrTable("nick",
+                "start", "weight", "height",
+                "gender", "age");
+    }
 }   // Main Class
